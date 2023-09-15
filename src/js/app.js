e6adb0dc2498eb551a07fbe19ec48fda782b3e7f
Ivonne <ivonnebenites2015@gmail.com>
2023-09-15 02:03:30 -0500

    Chore: add api and update file

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,19 @@
-export default class PlayerCollection {
-  constructor() {
-    this.players = [];
+/* eslint-disable import/prefer-default-export */
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0h9fPW0Emu3RZUlzfxoW/scores';
+
+export const listScore = async () => {
+  const response = await fetch(url);
+  const { result } = await response.json().catch((error) => new Error(error));
+  return result;
+};
+
+export const display = (list) => {
+  const listBox = document.querySelector('#listItems');
+  listBox.innerHTML = '';
+  if (list.length === 0) {
+    listBox.innerHTML = '<li> Sorry, no scores yet</li>';
   }
-
-  savePlayersToStorage() {
-    localStorage.setItem('players', JSON.stringify(this.players));
-  }
-
-  addPlayerToCollection(name, score) {
-    const player = {
-      id: Math.floor(Math.random() * 1000),
-      name,
-      score,
-    };
-    this.players.push(player);
-    this.savePlayersToStorage();
-  }
-
-  addPlayer(event) {
-    event.preventDefault(); // Prevent form submission and page refresh
-
-    const nameInput = document.getElementById('name');
-    const scoreInput = document.getElementById('score');
-
-    const name = nameInput.value.trim();
-    const score = scoreInput.value.trim();
-
-    if (name && score) {
-      this.getPlayersFromStorage(); // Retrieve existing players
-      this.addPlayerToCollection(name, score);
-      nameInput.value = '';
-      scoreInput.value = '';
-    }
-  }
-
-  getPlayersFromStorage() {
-    const storedPlayers = localStorage.getItem('players');
-    if (storedPlayers) {
-      this.players = JSON.parse(storedPlayers);
-    }
-  }
-}
-// PlayerCollection
-
-const playerCollection = new PlayerCollection();
-
-const form = document.getElementById('formPart');
-const submitButton = form.querySelector('button[type="submit"]');
-submitButton.addEventListener('click', (event) => playerCollection.addPlayer(event));
+  list.sort((a, b) => b.score - a.score).forEach((score) => {
+    listBox.innerHTML += `<li> ${score.user}: ${score.score}</li>`;
+  });
+};
